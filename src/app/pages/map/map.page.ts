2c8f561a5096120446d885142a7944d6017783a6
mavl <mvaronamac@gmail.coml>
2020-04-13 14:01:01 +0200

    Add map and marker

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+declare var mapboxgl: any;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
-export class MapPage implements OnInit {
+export class MapPage implements OnInit, AfterViewInit {
 
   lat: number;
   lng: number;
@@ -23,6 +25,67 @@ export class MapPage implements OnInit {
 
     console.log(this.lat, this.lng);
 
+  }
+
+  ngAfterViewInit(): void {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibXlybmFtaW5rb2ZmMTAiLCJhIjoiY2s4eG1yOGs4MDc1bzNtcDE4cHVxNjY3OSJ9.CYcKM_kPWTz5VFZ1CYupHg';
+    const map = new mapboxgl.Map({
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [this.lng, this.lat],
+      zoom: 15.5,
+      pitch: 45,
+      bearing: -17.6,
+      container: 'map',
+      antialias: true
+    });
+    map.on('load', () => {
+      map.resize();
+      new mapboxgl.Marker()
+         .setLngLat([ this.lng, this.lat])
+         .addTo(map);
+      const layers = map.getStyle().layers;
+      let labelLayerId;
+      // tslint:disable-next-line: prefer-for-of
+      for (let i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+          labelLayerId = layers[i].id;
+          break;
+        }
+      }
+      map.addLayer(
+        {
+          id: '3d-buildings',
+          source: 'composite',
+          'source-layer': 'building',
+          filter: ['==', 'extrude', 'true'],
+          type: 'fill-extrusion',
+          minzoom: 15,
+          paint: {
+            'fill-extrusion-color': '#aaa',
+            'fill-extrusion-height': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'height']
+            ],
+            'fill-extrusion-base': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'min_height']
+            ],
+            'fill-extrusion-opacity': 0.6
+          }
+        },
+        labelLayerId
+      );
+    });
   }
 
 }
